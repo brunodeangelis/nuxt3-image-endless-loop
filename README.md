e66a286e224496f2028fe6c365e547b2e82cfd1d
Bruno De Angelis <brunordeangelis@gmail.com>
2022-11-24 19:35:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Before testing this, be sure to fix the module as per PR [#661](https://github.com/nuxt/image/pull/661)
+
+Just run `yarn generate` and see the huge loop it creates in the output. This seems to be related to the `format="webp"` option
---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup>
+console.log("tst");
+</script>
+
+<template>
+  <div>
+    <main>
+      <NuxtImg
+        src="/1.jpg"
+        sizes="xs:480px sm:960px md:1152px"
+        format="webp"
+        loading="lazy"
+      />
+      <NuxtImg
+        src="/2.jpg"
+        sizes="xs:480px sm:960px md:1152px"
+        format="webp"
+        loading="lazy"
+      />
+    </main>
+  </div>
+</template>
